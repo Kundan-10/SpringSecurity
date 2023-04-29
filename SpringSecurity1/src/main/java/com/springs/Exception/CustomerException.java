f4c8fba99c2945ef51930c8d2c7654319da8d14c
Kundan-10 <kundanwakudkar10@gmail.com>
2023-04-29 17:46:34 +0530

    global exception added in exception

---
+++ SpringSecurity1/src/main/java/com/springs/Exception/CustomerException.java
@@ -0,0 +1,16 @@
+package com.springs.Exception;
+
+public class CustomerException extends Exception{
+	
+	public CustomerException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerException(String message) {
+		super(message);
+	}
+	
+	
+	
+
+}
