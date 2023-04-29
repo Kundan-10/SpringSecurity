543297b448d3cc5207ae4ea174ff61bb40d69e36
Kundan-10 <kundanwakudkar10@gmail.com>
2023-04-29 17:40:11 +0530

    myerror added in Exceptions

---
+++ SpringSecurity1/src/main/java/com/springs/Exception/CustomerExcpetion.java
@@ -0,0 +1,16 @@
+package com.springs.Exception;
+
+public class CustomerExcpetion extends Exception{
+	
+	public CustomerExcpetion() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerExcpetion(String message) {
+		super(message);
+	}
+	
+	
+	
+
+}
---
+++ SpringSecurity1/src/main/java/com/springs/Exception/MyErrorDetails.java
@@ -0,0 +1,5 @@
+package com.springs.Exception;
+
+public class MyErrorDetails {
+
+}
