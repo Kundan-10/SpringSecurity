48088a86be3b61c23a8fa7cc9451fda3515063b2
Kundan-10 <kundanwakudkar10@gmail.com>
2023-05-01 22:19:29 +0530

    getAllCustomerHander added in controller

--- SpringSecurity1/src/main/java/com/springs/controller/CustomerController.java
+++ SpringSecurity1/src/main/java/com/springs/controller/CustomerController.java
@@ -1,13 +1,18 @@
 package com.springs.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springs.Exception.CustomerException;
 import com.springs.model.Customer;
 import com.springs.service.CustomerService;
 
@@ -27,6 +32,7 @@ public class CustomerController {
 		
 	}
 	
+	@PostMapping("/customers")
 	public ResponseEntity<Customer> registerCustomer(@RequestBody Customer customer){
 		
 		customer.setPassword(passwordEncoder.encode(customer.getPassword()));
@@ -35,6 +41,15 @@ public class CustomerController {
 		
 	}
 	
+	@GetMapping("/customers/{email}")
+	public ResponseEntity<Customer> getCustomerDetailsByEmail(@PathVariable("email") String email) throws CustomerException{
+		Customer customer=customerService.getCustomerDetailsByEmail(email);
+		return new ResponseEntity<>(customer,HttpStatus.ACCEPTED);
+	}
 	
+	public ResponseEntity<List<Customer>>getAllCustomerHander() throws CustomerException{
+		List<Customer> customer=customerService.getAllCustomerDetails();
+		return new ResponseEntity<List<Customer>>(customer,HttpStatus.ACCEPTED);
+	}
 
 }
